76e51c56a53bb48956da8d7199a4c50b948565cf
Theo S <theo_s@berkeley.edu>
2022-02-22 15:10:13 -0800

    * Add plots for MAE + truncated RMSE for high dimensional experiments

---
+++ code/plot_simul_high_truncated.R
@@ -0,0 +1,111 @@
+library(ggplot2)
+library(dplyr)
+library(viridis)
+library(reshape2)
+library(scales)
+library(ggrepel)
+library(cowplot)
+library(ggpubr)
+
+plot_type = "trunc" #"trunc"
+
+if (plot_type == "mae") {
+  in_name = "code/high_mae.RDS"
+} else if (plot_type == "trunc"){
+  in_name = "code/high_mse_truncated.RDS"
+}
+
+mse_table <- readRDS(file = in_name)
+rmse_table <- mse_table
+if (plot_type == "trunc") {
+  rmse_table[,-c(1:4)] <- sqrt(rmse_table[,-c(1:4)])
+}
+
+rmse_table <- rmse_table[order(rmse_table$`Exp#`),]
+rmse_table <- rmse_table[order(rmse_table$`Exp#`, rmse_table$P, rmse_table$K, rmse_table$N),]
+
+linetypes <- c("NN Matching" = "dotted",
+               "LASSO" = "dotted",
+               "DR (lasso)" = "dashed",
+               "PS Matching" = "solid",
+               "HT" = "solid",
+               "HT (lasso)" = "solid",
+               "TMLE" = "dashed",
+               "CTMLE" = "dashed"
+)
+
+colors <- c("NN Matching" = "#88CCEE",
+            "LASSO" = "#AA4499",
+            "DR (lasso)" = "#999933",
+            "HT (lasso)" = "#6699CC",
+            "HT" = "#117733",
+            "PS Matching" = "#CC6677",
+            "TMLE" = "#661100",
+            "CTMLE" = "#661100"
+)
+
+col2 <- c("#88CCEE",
+          "#6699CC",
+          "#CC6677",
+          "#AA4499",
+          "#999933",
+          "#117733",
+          "#661100",
+          "#661100")
+
+
+for (i in 1:nrow(rmse_table)) {
+  k =rmse_table$K[i]
+  p = rmse_table$P[i]
+  exp = rmse_table$`Exp#`[i]
+
+  titlename <- paste0("Experiment ",exp,": K = ",k,", P = ",p)
+  filename <- paste0("code/figures/high_",plot_type,"_experiment",exp,"k",k,"p",10*p,".pdf")
+
+  if (plot_type == "trunc") {
+    ylabel = "Truncated RMSE when estimating ATE"
+  } else if (plot_type == "mae") {
+    ylabel = "MAE when estimating ATE"
+  }
+
+  rmse_table %>%
+    filter(K == k, P == p,`Exp#` == exp) %>%
+    ungroup() %>%
+    dplyr::select(-`K`, -`Exp#`,-P) %>%
+    melt(id.vars = "N") %>%
+    dplyr::rename(Estimator = variable) %>%
+    dplyr::filter(N == 1e4) -> end_values
+
+  end_values$Color = c("#661100",
+                       "#661100",
+                       "#88CCEE",
+                       "#AA4499",
+                       "#999933",
+                       "#6699CC",
+                       "#117733",
+                       "#CC6677")
+
+  rmse_table %>%
+    filter(K == k, P == p,`Exp#` == exp) %>%
+    ungroup() %>%
+    dplyr::select(-`K`, -`Exp#`,-P) %>%
+    melt(id = "N") %>%
+    dplyr::rename(Estimator = variable) %>%
+    ggplot(aes(x = N, y = value, color = Estimator, linetype = Estimator))+
+    geom_line(show.legend = FALSE)+
+    scale_linetype_manual(values = linetypes)+
+    scale_color_manual(values = colors)+
+    xlim(0, 11000) +
+    geom_text_repel(
+      aes(label = Estimator),
+      data = end_values, color = end_values$Color,
+      size = 2, force = 3,arrow = arrow(length = unit(0.01, "npc")),
+      direction = "both", nudge_x = 500, nudge_y = .005, point.padding = .72, max.overlaps = Inf
+    )+
+    theme_bw()+
+    theme(plot.title = element_text(hjust = 0.5))+
+    labs(y = ylabel, x = "Sample Size", title = titlename)
+
+  ggsave(filename = filename, height = 4, width = 4)
+
+}
---
+++ code/postprocess_high_truncated.R
@@ -0,0 +1,96 @@
+library(ggplot2)
+library(dplyr)
+library(viridis)
+
+all_data <- data.frame()
+
+
+for (file in dir("code/results_end")) {
+  print(file)
+  if (substr(file, 1,3) == "res") {
+    data <- readRDS(paste0("code/results_end/",file))
+    all_data <- rbind(all_data, data)
+  }
+  if (substr(file, 1,4) == "last") {
+    data <- readRDS(paste0("code/results_end/",file))
+    all_data <- rbind(all_data, data)
+  }
+}
+
+
+#all_data[order(all_data$Exp),]
+
+# Read in ATE's
+ate3 <- readRDS("~/Desktop/identification-not-enough/code/ate3.RDS")
+ate2 <- readRDS("~/Desktop/identification-not-enough/code/ate2.RDS")
+ate1 <- readRDS("~/Desktop/identification-not-enough/code/ate1.RDS")
+
+all_data$TrueATE <- NA
+
+ate1$Exp = 1
+ate2$Exp = 2
+ate3$Exp = 3
+
+all_data$N <- as.numeric(all_data$N)
+all_data$P <- as.numeric(all_data$P)
+all_data$K <- as.numeric(all_data$K)
+all_data$Exp <- as.numeric(all_data$Exp)
+
+# Fill true ATE's
+all_data <- dplyr::left_join(all_data, ate1, by = c("N","K","P","Exp"))
+all_data <- dplyr::left_join(all_data, ate2, by = c("N","K","P","Exp"))
+all_data <- dplyr::left_join(all_data, ate3, by = c("N","K","P","Exp"))
+
+all_data$TrueATE <- ifelse(!is.na(all_data$ATE.x), all_data$ATE.x,
+                           ifelse(!is.na(all_data$ATE.y), all_data$ATE.y,
+                                  ifelse(!is.na(all_data$ATE), all_data$ATE, NA)))
+
+all_data <- all_data %>% dplyr::select(-ATE,-ATE.x,-ATE.y)
+
+for (i in 1:ncol(all_data)) {
+  all_data[,i] <- as.numeric(all_data[,i])
+}
+
+all_data <- all_data[complete.cases(all_data),]
+
+# all_data[rowSums(is.na(all_data)) > 0,]
+
+all_data %>%
+  dplyr::filter(!is.na(tmle_c)) %>%
+  dplyr::mutate(across(-c(1:4), ~  . - TrueATE)) %>%
+  dplyr::mutate(across(-c(1:4), ~  . **2)) %>%
+  dplyr::select(-TrueATE) %>%
+  group_by(N,K,P,Exp) %>%
+  summarise(across(everything(), ~ mean(.x, na.rm = TRUE, trim = .025))) -> mse_table
+
+mse_table <- mse_table #%>% dplyr::select(-dr_lasso_cf_1,-ht_lasso_cf_1)
+
+colnames(mse_table) <- c("N","K","P","Exp#","TMLE","CTMLE","NN Matching",
+                         "LASSO", "DR (lasso)", "HT (lasso)","HT", "PS Matching")
+
+library(xtable)
+
+saveRDS(mse_table, file = "code/high_mse_truncated.RDS")
+
+rmse_table <- mse_table
+rmse_table[,-c(1:4)] <- sqrt(rmse_table[,-c(1:4)])
+
+rmse_table <- rmse_table[order(rmse_table$`Exp#`),]
+rmse_table <- rmse_table[order(rmse_table$`Exp#`, rmse_table$P, rmse_table$K, rmse_table$N),]
+
+xtable(rmse_table, digits = 4, caption = "Results for the RMSE in high dimensional simulations")
+
+
+all_data %>%
+  dplyr::filter(!is.na(tmle_c)) %>%
+  dplyr::mutate(across(-c(1:4), ~  . - TrueATE)) -> te
+
+abs(te) %>%
+  dplyr::select(-TrueATE) %>%
+  group_by(N,K,P,Exp) %>%
+  summarise(across(everything(), ~ mean(.x, na.rm = TRUE))) -> mae_table
+
+colnames(mae_table) <- c("N","K","P","Exp#","TMLE","CTMLE","NN Matching",
+                         "LASSO", "DR (lasso)", "HT (lasso)","HT", "PS Matching")
+
+saveRDS(mae_table, file = "code/high_mae.RDS")
